9fad16facffc5c0fba9f7f1dd61af82a6597a4fa
danirayGOD <mrdaniraymay@gmail.com>
2023-02-16 00:46:27 -0500

    terminado el bot seraphin

---
+++ main.js
@@ -0,0 +1,39 @@
+// const { Client } = require("discord.js");
+const { Configuration, OpenAIApi } = require("openai");
+const Discord = require("discord.js");
+const client = new Discord.Client({
+  intents: [
+    Discord.GatewayIntentBits.Guilds,
+    Discord.GatewayIntentBits.GuildMessages,
+    Discord.GatewayIntentBits.GuildMembers,
+    Discord.GatewayIntentBits.MessageContent,
+  ],
+});
+require("dotenv/config");
+const configuration = new Configuration({
+  apiKey: process.env.CHAT_GPT_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+client.on("ready", () => {
+  console.log(`Loggen is Complete :D ${client.user.tag}`);
+});
+client.on("messageCreate", async (message) => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith("-seraphin")) return;
+  message.content = message.content.replace("-seraphin ", "");
+
+  let prompt = `he thinks he's a seraphin, a higher rank than angel`;
+  prompt += `${message.author.username} : ${message.content}`;
+  const res = await openai.createCompletion({
+    model: "text-davinci-003",
+    max_tokens: 500,
+    // temperature: 0.9,
+    prompt,
+    stop: ["seraphin:", `${message.author.username}:`],
+  });
+
+  message.channel.send({ content: `${res.data.choices[0].text}` });
+});
+client.login(process.env.DISCORD_KEY);
